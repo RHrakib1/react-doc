9fcdfaa5818531e90982432d861c923c40423b3c
rakib hasan <rakibhasanmd457@gmail.com>
2025-05-30 08:49:28 +0600

    i have added to the react concept documentation

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Button from './new-component/btn'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-slate-100 flex justify-center items-center'>
+        <div>
+          <h1>React World</h1>
+          <p>Hello doc you are the best of the react world.I have realized this concept towmmorow</p>
+          <Button></Button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/new-component/btn.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+        <div>
+            <button type="submit" className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded">Submit</button>
+        </div>
+    )
+}
+
+export default Button;
