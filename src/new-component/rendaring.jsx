588c541dee31c598c4f0e366c5e61276be3fdb54
rakib hasan <rakibhasanmd457@gmail.com>
2025-05-31 08:51:29 +0600

    add to the style

--- src/new-component/rendaring.jsx
+++ src/new-component/rendaring.jsx
@@ -1,10 +1,14 @@
 const products = [
-    { title: 'Cabbage', id: 1 },
-    { title: 'Garlic', id: 2 },
-    { title: 'Apple', id: 3 },
+    { title: 'Cabbage', id: 1, isFruit: false, },
+    { title: 'Garlic', id: 2, isFruit: true, },
+    { title: 'Apple', id: 3, isFruit: false, },
 ];
+
+
 export default function Rendaring() {
-    const listItem = products.map(p => <li key={p.id}>
+    const listItem = products.map(p => <li key={p.id} style={{
+        color: p.isFruit ? 'magenta' : 'darkgreen'
+    }}>
         {p.title}
     </li>);
     return (
